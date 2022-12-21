143b79af48a1b34cbe1d55855147501778cce90a
cks612 <kyusung612@gmail.com>
2022-12-21 11:39:03 +0900

    [Fix] fixed axios baseUrl to localhost

--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -6,11 +6,11 @@ import { useGetNews } from "../../../hooks/news/useGetNews";
 const MainPage = () => {
   const cache = useQueryClient();
 
-  const { data } = useQuery(["news"], useGetNews);
-  // const data = cache.getQueryData(["news"]);
+  // const { data } = useQuery(["news"], useGetNews);
+  const data = cache.getQueryData(["news"]);
   // console.log(cache);
-  // console.log(data);
   console.log(data);
+  // console.log(data);
 
   return <div>fucking shit</div>;
 };
--- src/pages/api/news/index.ts
+++ src/pages/api/news/index.ts
@@ -3,21 +3,21 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { NAVER_CLIENT_ID, NAVER_CLIENT_SECRET } from "../../../utils/auth";
 
 const client = axios.create({
+  baseURL: "https://openapi.naver.com/v1",
   headers: {
     "X-Naver-Client-Id": NAVER_CLIENT_ID,
     "X-Naver-Client-Secret": NAVER_CLIENT_SECRET,
     Accept: "application/json",
-    "Accept-Encoding": "identity",
   },
   withCredentials: true,
 });
 
-const getNews = async (_req: NextApiRequest, res: NextApiResponse) => {
+const naverNews = async (_req: NextApiRequest, res: NextApiResponse) => {
   const { data } = await client.get(
-    `https://openapi.naver.com/v1/search/news?query="선한영향력"&display=20&start=1&sort=date`
+    `/search/news?query="선한영향력"&display=20&start=1&sort=date`
   );
 
   return res.status(200).json(data);
 };
 
-export default getNews;
+export default naverNews;
