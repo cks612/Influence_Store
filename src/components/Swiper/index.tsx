c32ccd495f1cb863d011652bbd013c3acb20bf1e
cks612 <kyusung612@gmail.com>
2022-12-21 15:16:56 +0900

    [ADD] newsCard components & Swiper library added
    뉴스카드 컴포넌트와 스와이퍼 라이브러리로 캐러셀 완성

---
+++ src/components/Swiper/index.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import ContentTitle from "../Content/ContentTitle";
+import { v4 as uuid_v4 } from "uuid";
+import { useQuery } from "@tanstack/react-query";
+import { useGetNews } from "../../hooks/news/useGetNews";
+import NewsCard from "../News/NewsCard";
+
+interface NewsDataProps {
+  title: string;
+  originallink: string;
+  pubDate: string;
+}
+
+const NewsSwiper = () => {
+  const { data: NewsData } = useQuery(["news"], useGetNews);
+
+  return (
+    <>
+      <ContentTitle>오늘의 뉴스</ContentTitle>
+      <Swiper
+        grabCursor={true}
+        slidesPerView={"auto"}
+        width={100}
+        scrollbar={{ draggable: true }}
+        className="mySwiper"
+      >
+        {NewsData.items.map((item: NewsDataProps) => (
+          <SwiperSlide key={uuid_v4()}>
+            <NewsCard item={item} />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
+  );
+};
+
+export default NewsSwiper;
--- src/components/pages/Main/index.tsx
+++ src/components/pages/Main/index.tsx
@@ -1,18 +1,15 @@
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-
 import React from "react";
-import { useGetNews } from "../../../hooks/news/useGetNews";
+import ContentWrapper from "../../Content/ContentWrapper";
+import NewsSwiper from "../../Swiper";
 
 const MainPage = () => {
-  const cache = useQueryClient();
-
-  // const { data } = useQuery(["news"], useGetNews);
-  const data = cache.getQueryData(["news"]);
-  // console.log(cache);
-  console.log(data);
-  // console.log(data);
-
-  return <div>fucking shit</div>;
+  return (
+    <>
+      <ContentWrapper>
+        <NewsSwiper />
+      </ContentWrapper>
+    </>
+  );
 };
 
 export default MainPage;
