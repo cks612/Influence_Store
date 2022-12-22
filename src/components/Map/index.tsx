4af9c3d31f23095f8603bcae93c46c594162cf3a
jungdeokwoo <dogsocks@dogsocksui-MacBookPro.local>
2022-12-22 17:22:45 +0900

    [ADD]marker added
    데이터배열을 통해 여러개의 마커를 찍을수 있도록 설정

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,17 +1,36 @@
-import React, { MutableRefObject, useEffect, useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import styled from "styled-components";
 import useGetCurrentLocation from "../../hooks/news/useGetCurrentLocation";
 
-type MapProps = { lat: string; lng: string };
+var POSITION = [
+  {
+    title: "카카오",
+    lat: 33.450705,
+    lng: 126.570677,
+  },
+  {
+    title: "생태연못",
+    lat: 33.450936,
+    lng: 126.569477,
+  },
+  {
+    title: "텃밭",
+    lat: 33.450879,
+    lng: 126.56994,
+  },
+  {
+    title: "근린공원",
+    lat: 33.451393,
+    lng: 126.570738,
+  },
+];
 
-const Map = ({ lat, lng }: MapProps) => {
+const Map = () => {
   const myLocation = useGetCurrentLocation();
   const kakaoMap = useRef<HTMLDivElement>(null);
-  const mapRef = useRef(null);
 
   const initMap = () => {
-    if (typeof myLocation === "string") return;
-
+    if (myLocation === null) return;
     const mapContainer = kakaoMap.current;
     const mapOption = {
       center: new kakao.maps.LatLng(myLocation.latitude, myLocation.longitude),
@@ -19,14 +38,31 @@ const Map = ({ lat, lng }: MapProps) => {
     };
 
     const map = new kakao.maps.Map(mapContainer as HTMLElement, mapOption);
-    (mapRef as MutableRefObject<any>).current = map;
+
+    map.setMinLevel(3);
+    map.setMaxLevel(8);
+
+    POSITION.forEach(item => {
+      const marker = new kakao.maps.Marker({
+        map: map,
+        position: new kakao.maps.LatLng(item.lat, item.lng),
+        clickable: true,
+      });
+      kakao.maps.event.addListener(marker, "click", function () {});
+    });
   };
 
   useEffect(() => {
     kakao.maps.load(() => initMap());
-  });
+  }, [myLocation]);
 
-  return <MapContainer ref={kakaoMap} />;
+  return (
+    <>
+      <MapContainer ref={kakaoMap}>
+        {!myLocation && <div>loading</div>}
+      </MapContainer>
+    </>
+  );
 };
 
 export default Map;
--- src/pages/store.tsx
+++ src/pages/store.tsx
@@ -8,7 +8,7 @@ const Store = () => {
   return (
     <StoreWrapper>
       <Search />
-      <StorePage />;
+      <StorePage />
     </StoreWrapper>
   );
 };
